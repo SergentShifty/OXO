fb70a3442176d2e89662dc7c00850c7c5937adc4
Belkaid Mehdi <belkaidmehdi93@gmail.com>
2016-03-01 07:40:01 +0100

    Board class created

---
+++ src/Board.java
@@ -0,0 +1,40 @@
+public class Board{
+	private Piece[][] _board;
+	
+	public Board(){
+		_board = new Piece[3][3];
+		for(int y = 0; y < _board.length; y++){
+			for(int x = 0; x < _board.length; x++){
+				_board[y][x] = Piece.EMPTY;
+			}
+		}
+	}
+	
+	public Piece getBoard(int y, int x){
+		if(y < 0 || x < 0 || y >= _board.length || x >= _board.length){
+			throw new IllegalArgumentException("Invalid arguments");
+		} else {
+			return _board[y][x];
+		}
+	}
+	
+	public void setBoard(int y, int x, Piece piece){
+		if(y < 0 || x < 0 || y >= _board.length || x >= _board.length){
+			throw new IllegalArgumentException("Invalid arguments");
+		} else {
+			_board[y][x] = piece;
+		}
+	}
+
+	@Override
+	public String toString(){
+		String text = "";
+		for(int y = 0; y < _board.length; y++){
+			for(int x = 0; x < _board.length; x++){
+				text += _board[y][x] + " ";
+			}
+			if(y < _board.length - 1) text += "\n";
+		}
+		return text;
+	}
+}
--- src/Piece.java
+++ src/Piece.java
@@ -1,5 +1,16 @@
 public enum Piece{
-	RED,
-	YELLOW,
-	EMPTY;
+	RED("X"),
+	YELLOW("O"),
+	EMPTY(".");
+	
+	private String _print;
+	
+	public Piece(String print){
+		_print = print;
+	}
+
+	@Override
+	public String toString(){
+		return _print;
+	}
 }
